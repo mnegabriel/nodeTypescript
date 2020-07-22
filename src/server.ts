870e96afdffce5640844d3241c0812df551f1e6b
Gabriel Dantas <gabrielmd123@gmail.com>
2020-07-22 09:06:37 -0300

    EsLint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-    return response.json( { message: "Rota inicial"} )
-})
-
+app.get('/', (request, response) => response.json({ message: 'Rota inicial' }));
 
 app.listen(3333, () => {
-    console.log(" 👏👏 Server started on port 3333")
-})
+  console.log(' 👏👏 Server started on port 3333');
+});
