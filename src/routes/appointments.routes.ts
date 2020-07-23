61952e857c7a20905fc0542ab8709cfffcf688f4
Gabriel Dantas <gabrielmd123@gmail.com>
2020-07-23 16:45:33 -0300

    appointment router initiated, initial date valiaation

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.get('/', (request, response) => {
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(appointment.date, parsedDate),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'Timeslot not available' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
