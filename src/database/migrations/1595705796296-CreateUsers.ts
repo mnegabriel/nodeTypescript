074b69c6a8bf5c193f3e78fe3b194f197d640f72
Gabriel Dantas <gabrielmd123@gmail.com>
2020-07-25 18:43:09 -0300

    some tweaks and relation created

--- src/database/migrations/1595705796296-CreateUsers.ts
+++ src/database/migrations/1595705796296-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1595705796296 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
