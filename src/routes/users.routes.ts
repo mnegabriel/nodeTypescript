e0d5cc3e69f029ef1050caf9fa3bed815dd0555b
Gabriel Dantas <gabrielmd123@gmail.com>
2020-07-26 16:55:09 -0300

    Applied file upload for avatar photo

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -18,5 +22,12 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => response.json({ inxi: 'ta ok bb' }),
+);
 
 export default usersRouter;
